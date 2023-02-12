4882d9cdf6a5e25979d6fef86e39795814163de9
darylbg <darylxcuf@gmail.com>
2023-02-12 12:29:26 +0000

    added window prompt and confirm selection criteria!

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 // Assignment Code
+var lowercaseletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+var specialCharachters = ["!", "#", "$", "%", "&", "(", ")", "*", "+"];
+var numerals = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("hey you clicked the button")
 //1a prompt the user for the password critiria
   var passwordLength = prompt("Please select the length of the password between 8 - 128 digits.");
-  var lowercaseCheck = confirm("Do you want this password to contain Lowercase letter?");
-  var uppercaseCheck = confirm("Do you wnat this password to contain uppercase letters?");
+
   //1b passwoard length btwn 8 - 125
   if ((passwordLength >= 8) && (passwordLength <= 128)) {
-    lowercaseCheck;
+    var lowercaseCheck = confirm("Do you want this password to contain Lowercase letter?");
   } else {
     alert("Please try again");
   }
 
+  if ((lowercaseCheck == true) || (lowercaseCheck == false)) {
+    var uppercaseCheck = confirm("Do you wnat this password to contain uppercase letters?");
+  } 
+
+  if ((uppercaseCheck == true) || (uppercaseCheck == false)) {
+    var specialCharachterCheck = confirm("Do you want this password to contain special charachters?");
+  } 
+
+  if ((specialCharachterCheck == true) || (specialCharachterCheck == false)) {
+    var numeralCheck = confirm("Do you want this password to contain numbers?");
+  }
+
+  if ((lowercaseCheck == false) && (uppercaseCheck == false) && (specialCharachterCheck == false) && (numeralCheck == false)) {
+    alert("You must select at least one option please try again");
+  }
+  
+  if ((lowercaseCheck == false) && (uppercaseCheck == false) && (specialCharachterCheck == false) && (numeralCheck == true)) {
+    var result = numerals.sort();
+  }
   
 
          
@@ -25,7 +50,7 @@ function generatePassword() {
 
 
 //4 display generated password 
-  return "password"
+  return result
 }
 
 // Write password to the #password input
